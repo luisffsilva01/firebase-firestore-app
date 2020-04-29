9d42b5d92465cc7880a58d8a8a8ba0258e7ea539
Luís Silva <luisffsilva01@gmail.com>
2020-04-29 18:43:21 +0100

    Login/Registar/Logout

--- app.js
+++ app.js
@@ -256,6 +256,75 @@ function logChanges() {
     });
  }
 
+ function login(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(error) {
+        console.log("Login com sucesso");
+        document.getElementById("btn-login").style = "display: none;"
+        document.getElementById("btn-registo").style = "display: none;"
+        document.getElementById("btn-logout").style = "display: block;"
+
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+              // User is signed in.
+              var displayName = user.displayName;
+              var email = user.email;
+              var emailVerified = user.emailVerified;
+              var photoURL = user.photoURL;
+              var isAnonymous = user.isAnonymous;
+              var uid = user.uid;
+              var providerData = user.providerData;
+              // ...
+            } else {
+              // User is signed out.
+              // ...
+            }
+          });
+        
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+ }
+
+ function registo(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if(email != "" && password != ""){
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            
+            var user = firebase.auth().currentUser;
+            user.sendEmailVerification().then(function() {
+            // Email sent.
+            }).catch(function(error) {
+            // An error happened.
+                console.log(error);
+            
+            });
+        });
+        
+    }
+    
+ }
+
+ function logout(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        document.getElementById("btn-login").style = "display: block;"
+        document.getElementById("btn-registo").style = "display: block;"
+        document.getElementById("btn-logout").style = "display: none;"
+      }).catch(function(error) {
+        // An error happened.
+      });
+ }
 // Chamada da função onLoad
 onLoad();
 logChanges();
